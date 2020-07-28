289388a28cdf2a7bf7c488f8c5bb6e72db5e5923
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2020-07-28 09:40:43 -0300

    Adicionando algumas alterações JS para a mudança de personagens no responsivo/mobile.

--- script/final.js
+++ script/final.js
@@ -1,12 +1,12 @@
 // Abrindo e fechando Menu-Lateral e adicionando o overlay no restante da página.
 function menuLateral() {
-    document.getElementById('menu-lateral').classList.toggle('active');
+    document.querySelector('.menu-lateral').classList.toggle('active');
 
     document.getElementById('overlay').classList.toggle('active');
 }
 
 function closeMenu() {
-    var btnClose = document.getElementById('menu-lateral').classList.toggle('active');
+    var btnClose = document.querySelector('.menu-lateral').classList.toggle('active');
 
     var closeOverlay = document.getElementById('overlay').classList.toggle('active');
 
@@ -26,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var slider_background = document.getElementById('backg');
     var image = ['background-ryu.jpg', 'background-ken.jpg', 'background-cloud.jpg', 'background-corrin.jpg', 'background-bay.jpg', 'background-richter.jpg'];
     var i = 2;
+
 
     
     var prevButton = document.querySelector('.prev');
@@ -85,7 +86,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 numberFighter.innerHTML = '#60';
                 numberFighter.style.backgroundImage = 'linear-gradient(225deg, #FFFBFB 0%, #FC8888 100%)';
                 numberFighter.style.left = '740px';
-
+                numberFighter.style.width = 'max-content'; 
                 //Ken
                 var nameKen = document.querySelector('.name-ken');
                 var plusKen = document.querySelector('.pluss');
@@ -141,8 +142,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 var nFightCorrin = document.querySelector('.nfight');
                 var descriptionUm = document.querySelector('.smashBros');
 
-                descriptionUm.style.top = '218px';
-                nameFighter.style.left = '0';
+                descriptionUm.style.top = '184px';
+                nameFighter.style.left = '175px';
                 nameFighter.style.transition = 'all 1s';
                 plusCloud.style.background = 'linear-gradient(225deg, #8c7abd 0%, #6d5b9c 100%)';
                 corMore.style.color = '#8c7abd';
@@ -230,6 +231,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    var prev = document.querySelector('.fighter__prev');
+    prev.addEventListener('click', function() {
+        if(i >= 1 && i <= image.length) {
+
+            stream.insertBefore(items[items.length - 1], items[0]);
+            items = document.querySelectorAll('.fighter__item');
+ 
+            i--;
+
+            return setImg();
+        }
+    });
+
     var nextButton = document.querySelector('.next');
     nextButton.addEventListener('click', function() {
 
@@ -295,7 +309,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 var nFightCloud = document.querySelector('.nfight-cloud');
                 var nFightCorrin = document.querySelector('.nfight');
 
-                nameCloud.style.left = '0';
+                nameCloud.style.left = '175px';
                 nameCloud.style.transition = 'all 1s';
                 plusCloud.style.backgroundImage = 'linear-gradient(225deg, #8c7abd 0%, #6d5b9c 100%)';
                 corNFight.style.color = '#8d78c0';
@@ -320,6 +334,52 @@ document.addEventListener('DOMContentLoaded', function() {
                 numberFighter.style.left = '748px';
                 numberFighter.style.top = '158px';
                 numberFighter.style.backgroundImage = 'linear-gradient(51deg, #e577e1 0%, #ffffff 100%)';
+
+                //Corrin
+                var nameCorrin = document.querySelector('.name-corrin');
+                var nameMale = document.querySelector('.name-male');
+                var plusCorrin = document.querySelector('.pluss');
+                var corMore = document.querySelector('.more');
+                var corNFight = document.querySelector('.nfight');
+                var nFight = document.querySelector('.n-fight');
+                var nFightBay = document.querySelector('.nfight-bay');
+                var hrCorrin = document.querySelector('.hr');
+                var fasLeft = document.querySelector('.fa-chevron-left');
+                var fasRight = document.querySelector('.fa-chevron-right');
+                var descriptionUm = document.querySelector('.smashBros');
+
+                descriptionUm.style.top = '280px';
+                nameCorrin.style.left = '3px';
+                nameCorrin.style.transition = 'all 1s';
+                nameCorrin.style.fontSize = '93px';
+                nameMale.style.opacity = '1';
+                nameMale.style.top = '412px';
+                plusCorrin.style.background = 'linear-gradient(224.76deg, rgba(255, 255, 255, 0.38) -5.62%, #B452AA 105.1%)';
+                corMore.style.color = '#EFBDF0';
+                corNFight.style.display = 'none';
+                nFight.style.marginLeft = '115px';
+                nFightBay.style.display = 'inline';
+                nFightBay.style.color = '#B957B0';
+                hrCorrin.style.border = '1px solid #EFBDF0';
+                fasLeft.style.color = '#B957B0';
+                fasRight.style.color = '#B957B0';
+            }
+
+            //Cloud -> Corrin
+            if (i == 3) {
+                //Cloud
+                var nameFighter = document.querySelector('.name-fighter');
+                var numberFighter = document.querySelector('.number-fighter');
+            
+
+                nameFighter.style.left = '-900px';
+                nameFighter.style.transition = 'all .7s';
+                numberFighter.innerHTML = '#62';
+                numberFighter.style.left = '748px';
+                numberFighter.style.top = '158px';
+                numberFighter.style.backgroundImage = 'linear-gradient(51deg, #e577e1 0%, #ffffff 100%)';
+                numberFighter.style.width = 'max-content';
+
 
                 //Corrin
                 var nameCorrin = document.querySelector('.name-corrin');
@@ -433,6 +493,28 @@ document.addEventListener('DOMContentLoaded', function() {
             return setImg();
         }
     });
+
+    var next = document.querySelector('.fighter__next');
+    next.addEventListener('click', function() {
+        if(i >= 0 && i <= image.length - 2) {
+            stream.appendChild(items[0]);
+            items = document.querySelectorAll('.fighter__item');
+
+            i++;
+
+            // Cloud -> Corrin
+            if(i == 3) {
+                // Cloud
+                
+                var nCloud = getElementById('n__cloud');
+
+                nCloud.style.opacity = '0';
+            }
+
+            return setImg();
+        }
+    });
+
 
     function setImg() {
         return slider_background.setAttribute('src', 'images/background/' + image[i]);
--- script/responsive.js
+++ script/responsive.js
@@ -1,70 +1,48 @@
-// Evento que faz a troca dos personagens
+// Abrindo e fechando Menu-Lateral e adicionando o overlay no restante da página.
+function menuLateral() {
+    document.querySelector('.menu-lateral').classList.toggle('active');
+
+    document.getElementById('overlay').classList.toggle('active');
+}
+
+function closeMenu() {
+    var btnClose = document.querySelector('.menu-lateral').classList.toggle('active');
+
+    var closeOverlay = document.getElementById('overlay').classList.toggle('active');
+
+    if(btnClose.hasAttribute('active') && closeOverlay.hasAttribute('active')) {
+        btnClose.removeAttribute('active');
+        closeOverlay.removeAttribute('active');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
     var stream = document.querySelector('.fighter__stream');
     var items = document.querySelectorAll('.fighter__item');
 
-    var sliderBackground = document.getElementById('backg-responsive');
-    var image = ['bg-ryu.jpg', 'bg-ken.jpg', 'bg-cloud.jpg', 'bg-corrin.jpg', 'bg-bay.jpg', 'bg-richter.jpg'];
-
+    var slider_background = document.getElementById('i_backG');
+    var image = ['background-ryu.jpg', 'background-ken.jpg', 'background-cloud.jpg', 'background-corrin.jpg', 'background-bay.jpg', 'background-richter.jpg'];
     var i = 2;
 
-    var prev = document.querySelector('.fighter__prev');
-    prev.addEventListener('click', function() {
-        if(i >= 1 && i <= image.length) {
+    var nextButton = document.querySelector('.next');
+    nextButton.addEventListener('click', function() {
 
-            stream.insertBefore(items[items.length - 1], items[0]);
-            items = document.querySelectorAll('.fighter__item');
- 
-            i--;
-
-            //Cloud <- Corrin
-            if (i == 2) {
-                //Corrin
-                var nameCorrin = document.querySelector('.name-corrin');
-
-                nameCorrin.style.left = '-1000px';
-                nameCorrin.style.transition = 'all 1s';
-
-                //Cloud
-                var nameFighter = document.querySelector('.name-fighter');
-
-                nameFighter.style.left = '0';
-                nameFighter.style.transition = 'all 1s';
-            }
-
-            return setImg();
-        }
-    });
-
-    var next = document.querySelector('.fighter__next');
-    next.addEventListener('click', function() {
         if(i >= 0 && i <= image.length - 2) {
-            stream.appendChild(items[0]);
-            items = document.querySelectorAll('.fighter__item');
 
-            i++;
-
-            //Cloud -> Corrin
             if (i == 3) {
                 //Cloud
                 var nameFighter = document.querySelector('.name-fighter');
+                var numberFighter = document.querySelector('.number-fighter');
 
-                nameFighter.style.left = '-1000px';
-                nameFighter.style.transition = 'all 1s';
-
-                //Corrin
-                var nameCorrin = document.querySelector('.name-corrin');
-
-                nameCorrin.style.left = '0';
-                nameCorrin.style.transition = 'all 1s';
+                nameFighter.style.left = '-900px';
+                nameFighter.style.transition = 'all .7s';
+                numberFighter.innerHTML = '#62';
+                numberFighter.style.left = '748px';
+                numberFighter.style.top = '158px';
+                numberFighter.style.backgroundImage = 'linear-gradient(51deg, #e577e1 0%, #ffffff 100%)';
             }
-
-            return setImg();
         }
-    });
 
-    function setImg() {
-        return sliderBackground.setAttribute('src', 'images/Mobile/background/' + image[i]);
-    }
+    });
 });
